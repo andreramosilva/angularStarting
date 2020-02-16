20ff08f1e57fb6f83677ce168c7b399122bf49d5
Andre Ramos da Silva <andre.ramos.da.silva@accenture.com>
2020-02-16 15:38:19 -0300

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { convertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./products/product-list.component";
+import { convertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
+import { StarComponent } from "./shared/star.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    convertToSpacesPipe
+    convertToSpacesPipe,
+    StarComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers:[],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
